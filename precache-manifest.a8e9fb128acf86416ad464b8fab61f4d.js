dbe98e4481081857e8aeced84a88a69063ca7751
YuliiaChupryna <julia.chuprina2907@gmail.com>
2019-12-19 18:12:22 +0200

    Updates

---
+++ precache-manifest.a8e9fb128acf86416ad464b8fab61f4d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6560f3a94efd2535b9849bf458f0852",
+    "url": "/react_dynamic-list-of-todos/index.html"
+  },
+  {
+    "revision": "375504346dcb0186d8fb",
+    "url": "/react_dynamic-list-of-todos/static/css/main.4508b880.chunk.css"
+  },
+  {
+    "revision": "55e58625568574069846",
+    "url": "/react_dynamic-list-of-todos/static/js/2.3811fce5.chunk.js"
+  },
+  {
+    "revision": "375504346dcb0186d8fb",
+    "url": "/react_dynamic-list-of-todos/static/js/main.7ec5c477.chunk.js"
+  },
+  {
+    "revision": "04977e40ef1cd22bcb98",
+    "url": "/react_dynamic-list-of-todos/static/js/runtime~main.b6f1ef06.js"
+  }
+]);
